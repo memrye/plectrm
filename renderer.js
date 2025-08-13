4f9cfc004460ee8eb3f9f653ff911415df2cabdd
memrye <bloab321@gmail.com>
2025-08-13 09:57:43 +0100

    Tweaked layout sizing

--- renderer.js
+++ renderer.js
@@ -1,10 +1,7 @@
-const func = async () => {
-  const response = await window.versions.ping()
-  console.log(response) // prints out 'pong'
-}
-
 const workspaceContext = document.getElementsByClassName('workspaceContainer').item(0);
+const ribbon = document.getElementsByClassName('ribbonContainer');
 
 new StaveBox(24, 'EADGBe')
 new TextBox()
 new StaveBox(16, 'EADGBe')
+
